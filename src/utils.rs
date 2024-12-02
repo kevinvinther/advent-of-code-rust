e3abec79fe9749a2cfb3d076e982332aa4b9dfa4
Kevin Vinther <kevinjoshua.kjv@gmail.com>
2024-12-02 11:14:25 +0100

    Add utility Function from String -> Vec<Vec<i32>>

--- src/utils.rs
+++ src/utils.rs
@@ -13,3 +13,15 @@ pub fn str_to_numbers(s: &str) -> Vec<i32> {
         .filter_map(|part| part.parse::<i32>().ok())
         .collect()
 }
+
+pub fn parse_to_vec_of_vec(s: &str) -> Vec<Vec<i32>> {
+    s.lines()
+        .filter(|line| !line.trim().is_empty())
+        .map(|line| {
+            line.trim()
+                .split_whitespace()
+                .filter_map(|num| num.parse::<i32>().ok())
+                .collect()
+        })
+        .collect()
+}
