c8d48072e469c4cd2954951fdaee984c83a3ada8
Kevin Vinther <kevinjoshua.kjv@gmail.com>
2024-12-04 09:33:26 +0100

    Add template for tests

---
+++ tests/template.rs
@@ -0,0 +1,15 @@
+// use advent_of_code_rust::year2024::day0x;
+
+// #[test]
+// fn test_part1_example() {
+//     let input = "\
+//     ";
+//     assert_eq!(day0x::part1(input), "xx");
+// }
+
+// #[test]
+// fn test_part2_example() {
+//     let input = "\
+//     ";
+//     assert_eq!(day0x::part2(input), "xx");
+// }
